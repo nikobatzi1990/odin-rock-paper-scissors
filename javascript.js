cc44e14859cdbc8a99bede07293847deaac06b3f
nikobatzi1990 <nicole.boci@gmail.com>
2022-09-19 21:59:30 +0900

    add main game functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,35 @@
 console.log("Hello World!");
+
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 3 + 1);
+    console.log(random);
+    if (random === 1) {
+        return "rock";
+    } else if (random === 2) {
+        return "paper";
+    } else if (random === 3) {
+        return "scissors";
+    }
+    
+}
+
+function playRound (playerSelection, computerSelection) {
+    //console.log(playerSelection.toLowerCase());
+    //console.log(computerSelection);
+    
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats Scissors!";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats Rock!"
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats Rock!";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats Paper!";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats Paper!"
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats Scissors!"
+    } else return "It's a draw!";
+}
+
+console.log(playRound("RoCk", getComputerChoice()));
